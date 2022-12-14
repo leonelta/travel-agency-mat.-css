43d373f0c1e53226288c2d5c291cbc59a624c91b
leonelta <ketchakaren@gmail.com>
2022-12-14 12:08:46 +0100

    setting height of all the images in the slider to 600

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const sideNav = document.querySelector('.sidenav');
-M.Sidenav.init(sideNav, {})
+M.Sidenav.init(sideNav, {});
+
+//slider
+
+const slider = document.querySelector('.slider');
+M.Slider.init(slider,{
+    indicator: false,
+    height: 600,
+})
