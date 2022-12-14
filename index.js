d1b433383bd4545f22fbf1925d7239df5944270c
leonelta <ketchakaren@gmail.com>
2022-12-14 13:36:29 +0100

    gave some styling to the class caption for it to be in the center

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ M.Sidenav.init(sideNav, {});
 const slider = document.querySelector('.slider');
 M.Slider.init(slider,{
     indicators: false,
-    height: 600,
+    height: 550,
     transition: 400,
     interval: 6000
 });
+
+//Autocomplete
+
+const ac = document.querySelector('.autocomplete')
+M.Autocomplete.init(ac, {
+    data: {
+        "Cameroon": null,
+        "Marrakech. Morrocco": null,
+        "Kenya": null,
+        "Cape-Town, South Africa": null,
+        "Tanzania": null,
+    }
+})
